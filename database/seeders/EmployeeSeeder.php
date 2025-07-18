391f2cad233874f0ca3da1678dace431f702851b
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-07-18 15:21:15 +0700

    add new datas seeder on employee and schedule

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -7,55 +7,158 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 
 class EmployeeSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('employees')->insert([
-            // Unit Personnel
-            [
-                'name' => 'Meranda',
-                'email' => 'randa@gmail.com',
-                'username' => 'randa',
-                'gender' => 'Wanita',
-                'address' => 'Jl. Mawar No. 12, Jakarta',
-                'phone_number' => '081234567890',
-                'division' => 'Unit Personnel',
-                'email_verified_at' => Carbon::now(),
-                'password' => Hash::make('foobarrr'),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Fikri Ramadhan',
-                'email' => 'fikri@gmail.com',
-                'username' => 'fikri',
+        $faker = Faker::create('id_ID');
+
+        $unitPersonnel = [
+            'Irsan F',
+            'Aldi I',
+            'Bambang P',
+            'M. Ardhika',
+            'Oktario',
+            'Reza P',
+            'Sepriando',
+            'Teguh S',
+            'Aser F',
+            'Muhammad Aldi',
+            'Reksa S',
+            'Rudi A',
+            'Okki P',
+            'Muhammad Hafiizh',
+            'Recky',
+            'Aldo F',
+            'Hardinata B',
+            'Julham C',
+            'Redho M',
+            'Satya A',
+            'Adam M',
+            'M Muarif',
+            'Adam E',
+            'Harry A',
+            'Muhammad Peado',
+            'M Hawari',
+            'Rahmat F',
+            'Tomi A'
+        ];
+
+        $wtpPersonnel = [
+            'Muhhamad Aldi',
+            'Reksa S',
+            'Syahru T',
+            'Olit R',
+            'M Ikhram',
+            'Tara dinata',
+            'Rendy F',
+            'Andi K',
+            'Hardinata B',
+            'Julham C',
+            'Renaldi',
+            'Satya A',
+            'Aliando S',
+            'Wheny S',
+            'M Afgan',
+            'Inggit E',
+            'Harianto A',
+            'M Padli',
+            'M Ahmad',
+            'Raisa F',
+            'Tonii A',
+            'Andhika P'
+        ];
+
+        $ashFgdPersonnel = [
+            'Tedi P',
+            'Aldi I',
+            'Prakoso A',
+            'M Lubi',
+            'Oktariansyah',
+            'Raditya',
+            'Suharman',
+            'Yazaman',
+            'Afriansyah',
+            'Alif Kurniawan',
+            'M Aldiandi',
+            'Reksa S',
+            'Rudi A',
+            'Olish Zuhir',
+            'M Okii',
+            'Burhanan',
+            'Rafli S',
+            'Aldo Paredo',
+            'Herlianto',
+            'Jumaidi',
+            'Rintan Anjani',
+            'Mario Teguh',
+            'M Zaki',
+            'Agusudin',
+            'M Ilkhlas',
+            'Aidil',
+            'Rizky P',
+            'Qafi P',
+            'Atthar M',
+            'Ralin Zapenia',
+            'Toni Pratama',
+            'Malik A'
+        ];
+
+        $allNames = array_merge($unitPersonnel, $wtpPersonnel, $ashFgdPersonnel);
+        $users = [];
+        $emailCounts = [];
+
+        $usernameCounts = []; // ← Tambahkan ini untuk menghindari username duplikat
+
+        foreach ($allNames as $index => $name) {
+            $baseUsername = Str::slug($name, '_');
+
+            // Tangani duplikat username
+            if (isset($usernameCounts[$baseUsername])) {
+                $usernameCounts[$baseUsername]++;
+                $username = $baseUsername . $usernameCounts[$baseUsername];
+            } else {
+                $usernameCounts[$baseUsername] = 1;
+                $username = $baseUsername;
+            }
+
+            // Tangani duplikat email berdasarkan username unik
+            if (isset($emailCounts[$username])) {
+                $emailCounts[$username]++;
+                $email = $username . $emailCounts[$username] . '@gmail.com';
+            } else {
+                $emailCounts[$username] = 1;
+                $email = $username . '@gmail.com';
+            }
+
+            if (in_array($name, $unitPersonnel)) {
+                $division = 'Unit Personnel';
+            } elseif (in_array($name, $wtpPersonnel)) {
+                $division = 'WTP Personnel';
+            } elseif (in_array($name, $ashFgdPersonnel)) {
+                $division = 'Ash FGD Personnel';
+            } else {
+                $division = 'Unknown';
+            }
+
+            $users[] = [
+                'name' => $name,
+                'email' => $email,
+                'username' => $username,
                 'gender' => 'Pria',
-                'address' => 'Jl. Merapi No. 27, Yogyakarta',
-                'phone_number' => '082198765441',
-                'division' => 'Ash FGD Personnel',
+                'address' => $faker->address,
+                'phone_number' => '08123' . str_pad($index + 1, 6, '0', STR_PAD_LEFT),
+                'division' => $division,
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('foobarrr'),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ],
-            [
-                'name' => 'Taufan',
-                'email' => 'taufan@gmail.com',
-                'username' => 'taufan',
-                'gender' => 'Pria',
-                'address' => 'Jl. Merapi No. 27, Yogyakarta',
-                'phone_number' => '082198765441',
-                'division' => 'WTP Personnel',
-                'email_verified_at' => Carbon::now(),
-                'password' => Hash::make('foobarrr'),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]
-        ]);
+            ];
+        }
+
+        DB::table('employees')->insert($users);
     }
 }
--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -4,47 +4,77 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 
 class ScheduleSeeder extends Seeder
 {
     public function run(): void
     {
-        // Employee ID dibagi berdasarkan divisi:
-        // Unit Personnel: ID 1 - 5
-        // WTP Personnel: ID 6 - 10
-        // Ash FGD Personnel: ID 11 - 15
-
-        $schedules = [
-           [
-                'employee_id' => 1,
-                'shift_id' => 1,
-                'date' => Carbon::now()->startOfWeek()->addDays(1)->setTime(7, 0),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'employee_id' => 1,
-                'shift_id' => 2,
-                'date' => Carbon::now()->startOfWeek()->addDays(2)->setTime(15, 0),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'employee_id' => 2,
-                'shift_id' => 3,
-                'date' => Carbon::now()->startOfWeek()->addDays(2)->setTime(15, 0),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'employee_id' => 3,
-                'shift_id' => 3,
-                'date' => Carbon::now()->startOfWeek()->addDays(3)->setTime(23, 0),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+        $unitPersonnel = [
+            'Irsan F', 'Aldi I', 'Bambang P', 'M. Ardhika', 'Oktario',
+            'Reza P', 'Sepriando', 'Teguh S', 'Aser F', 'Muhammad Aldi',
+            'Reksa S', 'Rudi A', 'Okki P', 'Muhammad Hafiizh', 'Recky',
+            'Aldo F', 'Hardinata B', 'Julham C', 'Redho M', 'Satya A',
+            'Adam M', 'M Muarif', 'Adam E', 'Harry A', 'Muhammad Peado',
+            'M Hawari', 'Rahmat F', 'Tomi A'
         ];
+
+        $wtpPersonnel = [
+            'Muhhamad Aldi', 'Reksa S', 'Syahru T', 'Olit R', 'M Ikhram',
+            'Tara dinata', 'Rendy F', 'Andi K', 'Hardinata B', 'Julham C',
+            'Renaldi', 'Satya A', 'Aliando S', 'Wheny S', 'M Afgan',
+            'Inggit E', 'Harianto A', 'M Padli', 'M Ahmad', 'Raisa F',
+            'Tonii A', 'Andhika P'
+        ];
+
+        $ashFgdPersonnel = [
+            'Tedi P', 'Aldi I', 'Prakoso A', 'M Lubi', 'Oktariansyah',
+            'Raditya', 'Suharman', 'Yazaman', 'Afriansyah', 'Alif Kurniawan',
+            'M Aldiandi', 'Reksa S', 'Rudi A', 'Olish Zuhir', 'M Okii',
+            'Burhanan', 'Rafli S', 'Aldo Paredo', 'Herlianto', 'Jumaidi',
+            'Rintan Anjani', 'Mario Teguh', 'M Zaki', 'Agusudin', 'M Ilkhlas',
+            'Aidil', 'Rizky P', 'Qafi P', 'Atthar M', 'Ralin Zapenia',
+            'Toni Pratama', 'Malik A'
+        ];
+
+        // Gabungkan semua dan map dengan divisinya
+        $allEmployees = [];
+
+        foreach ($unitPersonnel as $name) {
+            $allEmployees[] = ['name' => $name, 'division' => 'Unit'];
+        }
+
+        foreach ($wtpPersonnel as $name) {
+            $allEmployees[] = ['name' => $name, 'division' => 'WTP'];
+        }
+
+        foreach ($ashFgdPersonnel as $name) {
+            $allEmployees[] = ['name' => $name, 'division' => 'Ash FGD'];
+        }
+
+        // Hilangkan duplikat berdasarkan nama
+        $uniqueEmployees = collect($allEmployees)->unique('name')->values();
+
+        $shifts = [1, 2, 3]; // shift_id
+        $startDate = Carbon::now()->startOfWeek(); // Senin minggu ini
+        $schedules = [];
+
+        foreach ($uniqueEmployees as $index => $emp) {
+            $employee = DB::table('employees')->where('name', $emp['name'])->first();
+
+            if (!$employee) continue;
+
+            for ($i = 0; $i < 3; $i++) {
+                $schedules[] = [
+                    'employee_id' => $employee->id,
+                    'shift_id' => $shifts[$i],
+                    'date' => $startDate->copy()->addDays(($index + $i) % 7)->setTime([7, 15, 23][$i], 0),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
 
         DB::table('schedules')->insert($schedules);
     }
